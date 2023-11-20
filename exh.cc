82709fb5434eb16b6643139deeae1cf182382342
Ramon Ventura <83663129+RamonV007@users.noreply.github.com>
2023-11-20 12:19:33 +0100

    Add files via upload

--- exh.cc
+++ exh.cc
@@ -13,9 +13,20 @@ struct Player {
   int points;
 };
 
+struct Team {
+  int points = 0;
+  int price = 0;
+  // int/string/vector<int> tactic;
+};
+
+string database;
+string input_file;
+string output_file;
 vector<Player> players;
 uint goal = 1;
 uint def, mid, str, total_limit, player_limit;
+double start_time, end_time;
+double now() { return clock() / double(CLOCKS_PER_SEC); }
 
 void read_input(const string &database, const string &input_file) {
   ifstream in(database); // Reading database
@@ -54,10 +65,30 @@ void read_input(const string &database, const string &input_file) {
   in.close();
 }
 
+void print_output(string &output_file) {
+  ofstream out(output_file);
+  out.setf(ios::fixed);
+  out.precision(3);
+
+  end_time = now();
+  double time = end_time - start_time;
+
+  // Escriu solució
+  out << time << endl;
+  cout << time << endl;
+
+  out.close();
+}
+
+void exhaustive_search() { print_output(output_file); }
+
 int main(int argc, char **argv) {
-  string database = argv[1];
-  string input_file = argv[2];
-  string output_file = argv[3];
+  database = argv[1];
+  input_file = argv[2];
+  output_file = argv[3];
 
   read_input(database, input_file);
+
+  start_time = now();
+  exhaustive_search();
 }
