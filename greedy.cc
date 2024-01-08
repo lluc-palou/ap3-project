766e383ed1105f10e64f42c6477c4ce73c718c98
Ramon Ventura <83663129+RamonV007@users.noreply.github.com>
2024-01-08 19:06:40 +0100

    Add files via upload

--- greedy.cc
+++ greedy.cc
@@ -1,8 +1,5 @@
-/* IDEA: Ordenar database per ratio punts/preu + posició?
-   - Modificar la funció compare_players_efficiency per a deixar al final de la
-     database aquelles posicions ja cobertes.
-   - Ordenació de la database iterativa.
-*/
+// Greedy Algorithm.
+// Authors: Lluc Palou and Ramon Ventura.
 
 #include <algorithm>
 #include <cmath>
@@ -21,7 +18,6 @@ struct Player {
   int points;
 };
 
-// Arguments passed in code execution.
 string data_base;
 string query;
 string output_file;
@@ -32,9 +28,13 @@ int def, mig, dav, por, total_limit, player_limit;
 double start_time, end_time;
 double now() { return clock() / double(CLOCKS_PER_SEC); }
 
-/* Returns the player with largest points per price ratio, used to sort the
-player database. */
+/* Returns the most efficient player based on the following criteria, used to
+sort the player database. */
 bool compare_players_efficiency(const Player &a, const Player &b) {
+  /* Ordering criteria: Efficciency points/price ratio with a penalization to
+     expensive players. By manually experimenting with roots, the power
+     function (0.35) has been identified as the most effective. */
+
   // If both players have 0 points, order them based on price.
   if (a.points == 0 and b.points == 0) {
     return a.price < b.price;
@@ -45,15 +45,16 @@ bool compare_players_efficiency(const Player &a, const Player &b) {
     return false;
   } else if (b.points == 0) {
     return true;
+
   } else {
     double efficiency_a = (a.points * 1.0) / pow(a.price, 0.35);
     double efficiency_b = (b.points * 1.0) / pow(b.price, 0.35);
-
     return efficiency_a > efficiency_b;
   }
 }
 
-// Reads the soccer player database.
+/* Reads and stores the soccer player database. Discards players that surpass
+   the player limit price. */
 void read_data_base() {
   ifstream in(data_base);
   while (not in.eof()) {
@@ -75,7 +76,7 @@ void read_data_base() {
   }
   in.close();
 
-  // Sort database from most points scored to least
+  // Sorts database accordingly to the ordering criteria.
   sort(players.begin(), players.end(), compare_players_efficiency);
 }
 
@@ -87,91 +88,77 @@ void read_query() {
   in.close();
 }
 
-// Writes the best solution found by the algorithm till now in the output file.
+/* Auxiliar printing function. Given a position prints all the players from the
+solution that belong to it, in the correct format. */
+void aux_write_solution(ofstream &out,
+                        unordered_map<string, vector<string>> &players_position,
+                        const string &position) {
+  bool first = true;
+  for (const string &name : players_position[position]) {
+    if (!first) {
+      out << ";" << name;
+    } else {
+      out << name;
+      first = false;
+    }
+  }
+  out << endl;
+}
+
+// Given a solution, ends timing and prints it in the required format.
 void write_solution(const int &current_price, const int &current_points,
                     const vector<Player> &partial_solution) {
-  /*
-  // Sorts the solution based on soccer player positions for better formatting.
-  sort(partial_solution.begin(), partial_solution.end(),
-  compare_players_position);
-
-  // Returns the player by lexicographical order.
-  bool compare_players_position(const Player &a, const Player &b) {
-      return a.position < b.position;
-  }
-  */
-
   ofstream out(output_file);
   out.setf(ios::fixed);
   out.precision(1);
 
-  // Solution timing.
   end_time = now();
   double time = end_time - start_time;
-
-  // Outputs the time for this solution.
-  cout << "Found a solution in " << time << " seconds with " << current_points
-       << " points!" << endl;
   out << time << endl;
 
-  // Creates a map to group soccer players by position.
+  // Creates a map to group soccer players by position and fills the map.
   unordered_map<string, vector<string>> players_position;
-
-  // Fills the map.
   for (const Player &player : partial_solution) {
     players_position[player.position].push_back(player.name);
   }
 
-  // Writes the tactic soccer players by positions.
-  out << "POR: " << players_position["por"][0] << endl;
-
+  out << "POR: ";
+  aux_write_solution(out, players_position, "por");
   out << "DEF: ";
-  for (const string &name : players_position["def"]) {
-    out << name << ";";
-  }
-  out << endl;
-
+  aux_write_solution(out, players_position, "def");
   out << "MIG: ";
-  for (const string &name : players_position["mig"]) {
-    out << name << ";";
-  }
-  out << endl;
-
+  aux_write_solution(out, players_position, "mig");
   out << "DAV: ";
-  for (const string &name : players_position["dav"]) {
-    out << name << ";";
-  }
-  out << endl;
+  aux_write_solution(out, players_position, "dav");
 
-  // Writes best solution achieved points and price.
   out << "Punts: " << current_points << endl;
   out << "Preu: " << current_price << endl;
 
   out.close();
 }
 
-// Main algorithm concerning a greedy approach.
+/* Main algorithm concerning a greedy approach. Finds the first 11 players,
+   ordered by defined criteria, that meet the constraints. */
 void greedy_search(int def_count, int mig_count, int dav_count, int por_count,
                    int current_price, int current_points, vector<bool> &used,
                    vector<Player> &partial_solution) {
   int selected_players = 0;
   while (selected_players < 11) {
-    // Iterate through the players starting from index 'idx'.
     for (int i = 0; i < int(players.size()); ++i) {
-      // Skip used players.
-      if (not used[i] and players[i].price <= player_limit and
-          current_price + players[i].price < total_limit) {
-        // Check if adding the player satisfies the position constraints.
+      /* Skips used players, as well as the ones that do not meet the price
+         constraints. */
+      if (not used[i] and current_price + players[i].price < total_limit) {
+        // Checks if adding the player satisfies the position constraints.
         if ((players[i].position == "def" and def_count < def) or
             (players[i].position == "mig" and mig_count < mig) or
             (players[i].position == "dav" and dav_count < dav) or
             (players[i].position == "por" and por_count == 0)) {
 
-          // Add the player to the team.
+          // Adds the player to the team.
           used[i] = true;
           partial_solution.push_back(players[i]);
 
-          // Update counters, prices, and points.
+          // Updates counters, prices, and points.
           def_count += (players[i].position == "def");
           mig_count += (players[i].position == "mig");
           dav_count += (players[i].position == "dav");
@@ -188,19 +175,18 @@ void greedy_search(int def_count, int mig_count, int dav_count, int por_count,
 }
 
 int main(int argc, char **argv) {
-  // Arguments passed in code execution.
   data_base = argv[1];
   query = argv[2];
   output_file = argv[3];
 
-  // Reads the input files.
+  /* Firstly reads the query to store player limit. Allows us to filter them
+     during the database reading process. */
   read_query();
   read_data_base();
 
-  // Algorithm execution, solution writting, and timing.
+  // Algorithm execution.
   start_time = now();
   vector<Player> partial_solution;
-  // Vector to mark used soccer players in the exhaustive search.
   vector<bool> used(players.size(), false);
   greedy_search(0, 0, 0, 0, 0, 0, used, partial_solution);
 }
